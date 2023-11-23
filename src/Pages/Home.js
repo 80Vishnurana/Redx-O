b0a9ea2713f4098aa0c451e654353f25f90d6fa4
80Vishnurana <132250001+80Vishnurana@users.noreply.github.com>
2023-11-23 14:33:27 +0530

    23-11-2023

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useRef, useEffect } from 'react';
 import { register } from 'swiper/element/bundle';
 import { Grid, Typography, Box, Stack } from "@mui/material";
-import { useTheme, styled } from '@mui/material/styles';
+import { useTheme, styled, duration } from '@mui/material/styles';
 import images from "../core/config/homePage/SliderImages";
 import cards from "../core/config/homePage/FeatureCards";
 import { section } from "../core/config/homePage/FeatureCards";
@@ -12,6 +12,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Container from '@mui/material/Container';
+import data from "../core/config/homePage/whatWeDo";
 
 
 register();
@@ -43,9 +44,20 @@ const Home = () => {
 
     const StyledCard = styled(Card)(({ theme }) => ({
         '&:hover': {
-            boxShadow: `0 8px 16px ${theme.palette.primary.main}50`, // Add a box shadow on hover
+            boxShadow: `0 10px 20px ${theme.palette.primary.main}50`, // Add a box shadow on hover
             transform: 'scale(1.05)', // Increase size on hover
             transition: '0.3s ease-in-out', // Add a smooth transition effect
+        },
+    }));
+    const StyledBox = styled(Box)(({ theme }) => ({
+        height:'450px',
+        width:'500px',
+        border:'1px solid red',
+        '&:hover': {
+            boxShadow: `0 8px 16px ${theme.palette.primary.main}50`, // Add a box shadow on hover
+            transform: 'scale(1.05)', // Increase size on hover
+            transition: '0.3s ease-in-out', // Add a smooth transition effect        
+            transitionDuration: '0.5s',
         },
     }));
 
@@ -153,7 +165,28 @@ const Home = () => {
                     ))}
                 </Grid>
             </Container>
-
+            <Container sx={{bgcolor:'#f5f5f5', mt:4}} maxWidth="xl"> 
+            {data.map((data , index) =>(        
+                <Grid container spacing={2}  sx={{p:10,}} >             
+                    <Grid item xs={12} sm={6} md={6} >
+                        <StyledBox >
+                    <img src={data.img} alt="" style={{ height: '100%', width: '100%',  }}  />
+                        </StyledBox>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6} sx={{p:5}}>
+                        <Typography variant="h4" sx={{ color: `${theme.palette.primary.main}`,p:3 , }}>
+                          {data.heading}                  
+                        </Typography>
+                        <Typography sx={{py:3,p:3 ,fontSize:'20px' ,}} >
+                           {data.text}<br/>
+                        </Typography>
+                    </Grid>                 
+                </Grid>
+                  ))}      
+            </Container>
+            <Container sx={{mt:4}} maxWidth="xl"> 
+           
+            </Container>
         </>
     )
 }
